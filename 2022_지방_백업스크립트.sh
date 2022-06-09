6a1c790fe48ab6a48c4724bef1135f5027daefb7
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2022-06-09 22:41:15 +0900

    Update 2022_지방_백업스크립트.sh

--- 2022_지방_백업스크립트.sh
+++ 2022_지방_백업스크립트.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-time=`date "+%Y-%m-%d-%H:%M:%S"`;
+time=`date "+%Y-%m-%d-%H:%M:%S"`; #시간 포맷 설정
 
-`mkdir /backup 2> /dev/null`;
-`tar -g /var/log/* -zcvf /backup/$time-log.tar.gz`;
+`mkdir /backup 2> /dev/null`; #/backup 디렉토리 생성 , 표준 오류는 출력 X
+`tar -g /var/log/* -zcvf /backup/$time-log.tar.gz`; #시간-log.tar.gz 형식으로 압축
