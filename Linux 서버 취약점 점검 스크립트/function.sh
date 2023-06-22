ad42be0a7be40cdbf580c56874c49e2c7c664ab9
rkadl9999 <rkadl9999@naver.com>
2023-06-22 08:58:10 +0000

    ✅ :: Updated function.sh

--- Linux 서버 취약점 점검 스크립트/function.sh
+++ Linux 서버 취약점 점검 스크립트/function.sh
@@ -28,3 +28,72 @@ echo -e '\033[35m'"[ 정보 ] : $*"'\033[0m'
 CODE() {
 echo -e '\033[36m'$*'\033[0m'
 } >> $RESULT
+
+SCRIPTNAME() {
+basename $0 | awk -F. '{print $1}'
+}
+
+FindPatternReturnValue() {
+if egrep -v '^#|^$' $1 | grep -q $2 ; then
+	ReturnValue=$(egrep -v '^#|^$' $1 | grep $2 | awk -F= '{print $2}')
+else
+	ReturnValue=None
+fi
+echo $ReturnValue
+}
+
+IsFindPattern() {
+if egrep -v '^#|^$' $1 | grep -q $2 ; then
+	ReturnValue=$?
+else
+	ReturnValue=$?
+fi
+echo $ReturnValue
+}
+
+PAM_FindPatternReturnValue() {
+PAM_FILE=$1
+PAM_MODULE=$2
+PAM_FindPattern=$3
+LINE=$(egrep -v '^#|^$' $PAM_FILE | grep $PAM_MODULE)
+if [ -z "$LINE" ] ; then
+	ReturnValue=None
+else
+	PARAMS=$(echo $LINE | cut -d ' ' -f4-)
+	set $PARAMS
+	while [ $# -ge 1 ]
+	do
+		CHOICE1=$(echo $* | awk '{print $1}' | awk -F= '{print $1}')
+		CHOICE2=$(echo $* | awk '{print $1}' | awk -F= '{print $2}')
+		case $CHOICE1 in
+			$PAM_FindPattern) ReturnValue=$CHOICE2 ;;
+			*) : ;;
+		esac
+		shift
+	done
+fi
+echo $ReturnValue
+}
+
+CheckEncryptedPasswd() {
+$FILE=$1
+EncryptedPasswdField=$(grep '^root' $FILE | awk -F: '{print $2}' | awk -F'$' '{print $2}')
+case $EncryptedPasswdField in
+	1|2a|5) echo WarnTrue ;;
+	6) echo TrueTrue ;;
+	*) echo 'None' ;;
+esac
+}
+
+SearchValue() {
+SEARCH=$(egrep -v '^#|^$' $2 | sed 's/#.*//' | grep -w $3)
+if [ -z "$SEARCH" ] ; then
+	echo FALSE
+else
+	if [ $1 = 'KEYVALUE' ] ; then
+	echo $SEARCH
+elif [ $1 = 'VALUE' ] ; then
+	echo "$SEARCH" | awk '{print $2}'
+	fi
+fi
+}
